d6f4870defbd44b05a8e47aec71ee0c7a0b39f6a
Administrator <Administrator@WIN-2TC2K8MP962>
2026-02-01 12:32:03 +0200

    UP

--- ApiTest/Controllers/AccountController.cs
+++ ApiTest/Controllers/AccountController.cs
@@ -3,6 +3,10 @@ using ApiTest.Data.Model;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace ApiTest.Controllers
@@ -55,9 +59,48 @@ namespace ApiTest.Controllers
             if (GetName != null)
             {
                 bool found = await _userManager.CheckPasswordAsync(GetName, registerdto.Password);
-                if(found)
+                if (found)
                 {
                     // Genrate JWT
+
+                    // Create PayLoad (Claims)
+
+                    List<Claim> UserCliam = new List<Claim>();
+
+                    UserCliam.Add(new Claim(ClaimTypes.Name, GetName.UserName));
+                    UserCliam.Add(new Claim(ClaimTypes.NameIdentifier, GetName.Id));
+                    UserCliam.Add(new Claim(ClaimTypes.Email, GetName.Email));
+                     var Rols = await _userManager.GetRolesAsync(GetName);
+                    foreach (var rolitem in Rols)
+                    {
+                        UserCliam.Add(new Claim(ClaimTypes.Role, rolitem));
+                    }
+                    // Generete Guid In ID Claim To Not Static Token 
+
+                    UserCliam.Add(new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()));
+
+                    // Create Signture (signingCredentials)
+                    var Key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("123456789qwertyuioplkjhgfdsazxcvbnmQWERTYUIOPLKJHGFDSAZXCVBNM!@#$%^&*)(?><:+_"));
+                    SigningCredentials signingCred = new SigningCredentials(
+                        Key, SecurityAlgorithms.HmacSha256
+                        );
+
+                    // Design the token
+                    JwtSecurityToken mytoken = new JwtSecurityToken(
+                        // Here you can add claims, issuer, audience, expiration, and signing credentials
+                        issuer: "https://localhost:7148/",  // BaeseUrl
+                        audience: "https://localhost:4200/",  // Angluer
+                        expires: DateTime.Now.AddHours(1),
+                        claims: UserCliam,
+                        signingCredentials: signingCred
+
+                    );
+
+                    return Ok(new
+                    { //JwtSecurityTokenHandler  To Generate Token And Valid
+                        token = new JwtSecurityTokenHandler().WriteToken(mytoken),
+                        expiration = mytoken.ValidTo
+                    });
 
                 }
                 ModelState.AddModelError(string.Empty, "UserName Or Password Is Error");
